1a5886eae2a39bb878f5a7b8b0fcbd1f467f2f0d
Laura Badgley <l.badgleyy@gmail.com>
2019-06-10 14:27:45 -0700

    heroku commit

---
+++ tests/notes.test.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const request = require('supertest');
+const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect');
+const app = require('../lib/app');
+
+describe('notes routes', () => {
+  beforeAll(() => {
+    return connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connect.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('can create a note via POST', () => {
+    return request(app)
+      .post('/api/v1/notes')
+      .send({ title: 'the title', body: 'the body of the note' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          title: 'the title',
+          body: 'the body of the note',
+          __v: 0
+        });
+      });
+  });
+});

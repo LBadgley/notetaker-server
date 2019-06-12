681b3c874a3271bbd299d6e69cc40d82c393289b
Laura Badgley <l.badgleyy@gmail.com>
2019-06-11 18:52:45 -0700

    added ensure-auth middleware

--- tests/notes.test.js
+++ tests/notes.test.js
@@ -4,6 +4,13 @@ const mongoose = require('mongoose');
 const connect = require('../lib/utils/connect');
 const app = require('../lib/app');
 
+const createNote = note => {
+  return request(app)
+    .post('/api/v1/notes')
+    .send(note)
+    .then(res => res.body);
+};
+
 describe('notes routes', () => {
   beforeAll(() => {
     return connect();
@@ -28,6 +35,20 @@ describe('notes routes', () => {
           body: 'the body of the note',
           __v: 0
         });
+      });
+  });
+  
+  it('can get a list of notes', async() => {
+    const notes = await Promise.all([
+      createNote({ title: 'title1', body: 'body1' }),
+      createNote({ title: 'title2', body: 'body2' }),
+      createNote({ title: 'title3', body: 'body3' })
+    ]);
+
+    return request(app)
+      .get('/api/v1/notes')
+      .then(res => {
+        expect(res.body).toEqual(notes);
       });
   });
 });
